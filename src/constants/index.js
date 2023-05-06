e1d22e06994d16eeff03bc5bc55d79aa407c9ca2
xscavx0622 <scavx69@gmail.com>
2023-05-06 22:06:35 +0300

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,263 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "User Interface Designer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Interior Design Architect",
+    company_name: "Freelance / Multiple Jobs",
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "October 2011 - December 2019",
+    points: [
+      "Communicating with homeowners, builders for freelance projects.",
+      "Supervised installation, assured customer satisfaction.",
+      "Designed floor maps using AutoCAD and 3D perspectives using 3Ds Max/Revit.",
+      "Drafting documents to ease communication between builders and contractors.",
+    ],
+  },
+  {
+    title: "Graphic Designer",
+    company_name: "Freelance",
+    icon: tesla,
+    iconBg: "#E6DEDD",
+    date: "June 2007 - May 2020 ",
+    points: [
+      "Lead designer in various branding and packaging projects.",
+      "In some projects, worked with the marketing team to analyze social media metrics. ",
+      "Responsible for projects and logo creation for small businesses, using Adobe Illustrator and Adobe Photoshop.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "Upwork",
+    icon: shopify,
+    iconBg: "#383E56",
+    date: "January 2023 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "User Interface Designer",
+    company_name: "Upwork",
+    icon: meta,
+    iconBg: "#E6DEDD",
+    date: "October 2022 - Present",
+    points: [
+      "Have worked on different learning projects and boot camps and designed various app user interface.",
+      "Graphic Design background using skills such as typography, branding, color theory ",
+      "Teamwork and communication skills.",
+      "Critical thinking and problem solving.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Blockchain Solutions",
+    description:
+      "For the Blockchain Solutions project, I designed a web-based blockchain management platform that enables users to manage and track their blockchain.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "figma",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://www.behance.net/gallery/169792287/Blockchain-Solution-UX-Mockup",
+  },
+  {
+    name: "Landing Page",
+    description:
+      "A modern sign in/sign up landing web page is a webpage designed to facilitate user registration and login to a website or application.",
+    tags: [
+      {
+        name: "javascript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "html",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "html",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "css",
+        color: "green-text-gradient",
+      },
+      {
+        name: "javascript",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
